6c84e5f20d38e1b3fc3d069a91a88283dd8ff30e
SeungPyo Han <dhffl1234@naver.com>
2023-07-07 17:10:51 +0900

    채팅 메시지 전송을 위한 엔터 키 기능 추가

--- src/js/chat.js
+++ src/js/chat.js
@@ -19,6 +19,12 @@ const send = () =>{
     chatInput.focus();
 }
 
+chatInput.addEventListener("keypress", (event)=>{
+    if(event.keyCode === 13){
+        send();
+    }
+});
+
 sendButton.addEventListener("click", send);
 
 socket.on("chatting", (data)=>{
